a009d659be1cd17ee6736120399cd60fec64c16c
ikedaema2018 <ikedaema0817@gmail.com>
2018-08-24 15:41:55 +0900

    緯度経度dポストのため1

--- app/controllers/locate_infos_controller.rb
+++ app/controllers/locate_infos_controller.rb
@@ -1,10 +1,10 @@
 class LocateInfosController < ApplicationController
   def create
-    put "testtest"
-    @locate_info = LocateInfo.new(create_params)
+    puts "testtest"
+    locate_info = LocateInfo.new(create_params)
 
     # エラー処理
-    unless memo.save # もし、memoが保存できなかったら
+    unless locate_info.save # もし、memoが保存できなかったら
       @error_message = [memo.errors.full_messages].compact # エラーが入ってるインスタンス変数を定義
     end
   end
