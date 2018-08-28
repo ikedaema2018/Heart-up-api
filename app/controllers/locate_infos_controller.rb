f99a4d4a73e90f6ca0dccec95a363c0135e271cc
ikedaema2018 <ikedaema0817@gmail.com>
2018-08-28 22:23:57 +0900

    get

--- app/controllers/locate_infos_controller.rb
+++ app/controllers/locate_infos_controller.rb
@@ -1,17 +1,19 @@
 class LocateInfosController < ApplicationController
 
   def index
-    @locate_info = LocateInfo.first
+    @locate_info = LocateInfo.all
     render json: @locate_info
   end
 
   def create
     puts "testtest"
-    locate_info = LocateInfo.new(create_params)
+    @locate_info = LocateInfo.new(create_params)
 
     # エラー処理
     unless locate_info.save # もし、memoが保存できなかったら
-      @error_message = [memo.errors.full_messages].compact # エラーが入ってるインスタンス変数を定義
+      render json:@error_message = [memo.errors.full_messages].compact # エラーが入ってるインスタンス変数を定義
+    else
+      # render json: @locate_info
     end
   end
 
