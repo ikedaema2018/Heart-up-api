dfe8cfa5334190a404c7f190ba32174152ef24d0
ikedaema2018 <ikedaema0817@gmail.com>
2018-09-01 16:44:22 +0900

    auth_tokenで識別する前、&& user_idを入れる前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :login_check
+
+  # private
+    def login_check
+      @user = User.find_by(auth_token: params["auth_token"])
+      if !@user.nil?
+        render status: 401, json: { status: 401, message: 'ユーザー情報がおかしい' }
+      end
+    end
 end
