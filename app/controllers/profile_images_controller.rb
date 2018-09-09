6e0f134501f2e024da7f97ad5af0580cad0613ad
ikedaema2018 <ikedaema0817@gmail.com>
2018-09-09 15:02:05 +0900

    画像の名前をデータベースに保存完了

--- app/controllers/profile_images_controller.rb
+++ app/controllers/profile_images_controller.rb
@@ -1,8 +1,10 @@
 class ProfileImagesController < ApplicationController
-  def create 
-    p "aadawdawdawwda"
+  def create
     uploaded_file = fileupload_params[:file]
-    output_path = Rails.root.join('public/profile_image', uploaded_file.original_filename)
+    @path_name = uploaded_file.original_filename
+    output_path = Rails.root.join('public/profile_image', @path_name)
+
+    p @path_name
     
     begin
       File.open(output_path, 'w+b') do |fp|
@@ -11,14 +13,15 @@ class ProfileImagesController < ApplicationController
     rescue
       head 500
     end
+
+
+    @profile_image = ProfileImage.new(create_params)
     
-    # @profile_image = ProfileImage.new(create_params)
-    
-    # if @profile_image.save
-    #   render :json @profile_image
-    # else 
-    #   render :json @profile_image.errors
-    # end
+    if @profile_image.save
+      head 200
+    else 
+      head 500
+    end
 
   end
 
@@ -31,6 +34,6 @@ class ProfileImagesController < ApplicationController
   end
 
   def create_params
-    params.merge(user_id: @user.id, image_path: output_path)
+    params.permit().merge(user_id: @user.id, image_path: @path_name)
   end
 end
