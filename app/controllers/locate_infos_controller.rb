51e4c3dac8a6247cd3398bb38dd723e368d8355b
ikedaema2018 <ikedaema0817@gmail.com>
2018-09-14 16:31:38 +0900

    悩みをポストした時に一番近い場所にいるユーザーを取得できた

--- app/controllers/locate_infos_controller.rb
+++ app/controllers/locate_infos_controller.rb
@@ -13,7 +13,9 @@ class LocateInfosController < ApplicationController
       render json:@error_message = [@locate_info.errors.full_messages].compact # エラーが入ってるインスタンス変数を定義
     else
       #user_locatesから一番近いユーザーを検索してtarget_userに登録
-      
+      user_id = close_of_distance(@locate_info[:ido], @locate_info[:keido])
+      target_user = TargetUser.new(user_id: user_id, locate_info_id: @locate_info[:id])
+      target_user.save
 
       render json: @locate_info
     end
@@ -32,34 +34,64 @@ class LocateInfosController < ApplicationController
   end
 
   def distance
-    # SQLを作成
-    sql = <<-"EOS"
-    select
-    a.id
-  , a.ido
-  , a.keido
-  , a.user_id
-  , ( 
-    6371 * ACOS( 
-      COS(RADIANS(37.3248)) * COS(RADIANS(a.ido)) * COS(RADIANS(a.keido) - RADIANS(-122.0197))
-       + SIN(RADIANS(37.3248)) * SIN(RADIANS(a.ido))
-    )
-  ) as DISTANCE 
-from
-  user_locates a ORDER BY DISTANCE LIMIT 1; 
-    EOS
-
-# sqlを実行し、取得結果をhashに変換
-# ActiveRecord::Base.connection.select_all(sql).to_hash     
-    
-    test2 = ActiveRecord::Base.connection.select_all(sql).to_hash 
-    p test2[0]["user_id"]
+    p close_of_distance(37.3307, -122.0299)
+    render json: @locate_infos
   end
+
+  
 
   private
   def create_params
     params.require(:locate).permit(:ido, :keido, :nayami, :color).merge(user_id: @user.id)
   end
+
+  #close_of?distanceのためのソナタ
+  def set_up(latitude, longitude)
+    sql4 = <<-"EOS"
+    (
+      6371 * ACOS(
+        COS(RADIANS(
+          EOS
+
+    sql5 = <<-"EOS"
+          )) * COS(RADIANS(a.ido)) * COS(RADIANS(a.keido) - RADIANS(
+          EOS
+
+    sql6 = ")) + SIN(RADIANS(#{latitude})) * SIN(RADIANS(a.ido))))"
+    sql2 = sql4 + latitude.to_s + sql5 + longitude.to_s + sql6
+    return sql2
+  end
+
+  #これで引数の緯度と経度に一番近いuser_idを取得
+  def close_of_distance(latitude, longitude)
+    p "test"
+    # SQLを作成
+    sql1 = <<-"EOS"
+    select
+    a.id
+  , a.ido
+  , a.keido
+  , a.user_id
+  , 
+  EOS
+      #ここにset_upの内容
+
+        sql3 = <<-"EOS"
+      as DISTANCE
+    from
+      user_locates a ORDER BY DISTANCE LIMIT 1;
+        EOS
+
+    # sqlを実行し、取得結果をhashに変換
+    # ActiveRecord::Base.connection.select_all(sql).to_hash 
+    sql2 = set_up(latitude, longitude)
+    sql = sql1 + sql2 + sql3
+        # return sql
+        
+        test2 = ActiveRecord::Base.connection.select_all(sql).to_hash 
+        return test2[0]["user_id"]
+  end
+
 
 
 end
