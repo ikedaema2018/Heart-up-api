c9cb59fc7f1ff19c203e10dd611648633847b8df
ikedaema2018 <ikedaema0817@gmail.com>
2018-09-22 15:10:34 +0900

    shabon_alert_sirializerを元に戻す

--- app/serializers/shabon_alert_serializer.rb
+++ app/serializers/shabon_alert_serializer.rb
@@ -5,14 +5,15 @@ class ShabonAlertSerializer < ActiveModel::Serializer
   # has_and_belongs_to_many :users
   belongs_to :locate_info
   
-  class LocateInfoSerializer < ActiveModel::Serializer
-    attributes :id,
-             :nayami,
-             :ido,
-             :keido,
-             :user_id,
-             :color,
-             :life_flag
-  belongs_to :user
-  end
+  # class LocateInfoSerializer < ActiveModel::Serializer
+  #   attributes :id,
+  #            :nayami,
+  #            :ido,
+  #            :keido,
+  #            :user_id,
+  #            :color,
+  #            :life_flag
+             
+  # belongs_to :user
+  # end
 end
