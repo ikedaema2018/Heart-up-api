451991c8876261c6ed20cfaec1a20444c56b5a7c
ikedaema2018 <ikedaema0817@gmail.com>
2018-09-27 10:57:08 +0900

    daw

--- lib/user_stalke.rb
+++ lib/user_stalke.rb
@@ -9,6 +9,7 @@ module UserStalke
       p f.target_user
       @user_id = f.target_user.user_id
       @user_locate = UserLocate.find_by(user_id: @user_id)
+      p "--------------------idokeido---------------------------------------"
        p "________________"
       p f.keido
       p @user_locate.keido
@@ -34,7 +35,7 @@ module UserStalke
         f.target_user.save
       elsif (f.ido.abs - @user_locate.ido.abs).abs <= 0.006
         p "緯度が0.005以内だよ！"
-        if f.ido - @user_locate.ido >= 0
+        if f.keido - @user_locate.keido >= 0
           f.keido -= 0.010
         else
           f.keido += 0.010
@@ -42,7 +43,7 @@ module UserStalke
         f.save
       elsif (f.keido.abs - @user_locate.keido.abs).abs <= 0.006
         p "経度が0.005以内だよ！"
-        if f.keido - @user_locate.keido >= 0
+        if f.ido - @user_locate.ido >= 0
           f.ido -= 0.010
         else
           f.ido += 0.010
@@ -52,19 +53,25 @@ module UserStalke
         p 'ダメダメ'
         if f.ido - @user_locate.ido >= 0.006
           f.ido -= 0.005
-        elsif @user_locate.ido - f.ido >= 0.006
+        else
           f.ido += 0.005
         end
         
         if f.keido - @user_locate.keido >= 0.006
           f.keido -= 0.005
-        elsif @user_locate.keido - f.keido >= 0.006
+        else
           f.keido += 0.005
         end
         f.save
       end
                   
     end
+    p f.keido
+    p @user_locate.keido
+
+    p "________________"
+    p f.ido
+    p @user_locate.ido
   end
 
   module_function :user_stalke
