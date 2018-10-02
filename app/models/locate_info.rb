548e73e2b785c26976996218bb3c188d44f1aea1
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-02 17:50:50 +0900

    modelとsirializerで関連付け

--- app/models/locate_info.rb
+++ app/models/locate_info.rb
@@ -2,6 +2,7 @@ class LocateInfo < ApplicationRecord
   belongs_to :user
   has_many :nayami_comments
   has_many :shabon_alerts
+  has_many :first_locates
   has_one :target_user
   has_one :splash_yonda_check
 end
