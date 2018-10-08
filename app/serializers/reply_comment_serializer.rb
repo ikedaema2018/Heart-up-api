265426bf1cd255f90d54cf71b0c94e1b14f96b42
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-08 15:51:58 +0900

    nayami_comment has_many reply_comments

--- app/serializers/reply_comment_serializer.rb
+++ app/serializers/reply_comment_serializer.rb
@@ -1,3 +1,6 @@
 class ReplyCommentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id,
+             :replt_comment,
+             :nayami_comment_id,
+             :user_id
 end
