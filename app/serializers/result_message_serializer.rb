4c8926c8c7434e49db962e8fa7e300235d149cc7
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-23 13:55:39 +0900

    修正

--- app/serializers/result_message_serializer.rb
+++ app/serializers/result_message_serializer.rb
@@ -1,6 +1,6 @@
 class ResultMessageSerializer < ActiveModel::Serializer
-  attributes :id
-             :locate_info_id
+  attributes :id,
+             :locate_info_id,
              :result_message
 
   belongs_to :locate_info
