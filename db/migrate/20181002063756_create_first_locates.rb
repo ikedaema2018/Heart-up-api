b72af4280d6823f62c869ac6a75aa0f6be9e72cd
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-30 15:52:11 +0900

    tete

--- db/migrate/20181002063756_create_first_locates.rb
+++ db/migrate/20181002063756_create_first_locates.rb
@@ -2,7 +2,6 @@ class CreateFirstLocates < ActiveRecord::Migration[5.1]
   def change
     create_table :first_locates do |t|
       t.integer :locate_info_id
-      t.integer :user_id
       t.float :ido
       t.float :keido
 
